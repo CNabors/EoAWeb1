a2eeef1fe0ecb6e9294df281b13b991a671963aa
CNabors <CNabors.SithLord@gmail.com>
2010-02-05 01:11:18 -0500

    First Working Version

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-{\rtf1}
+window.addEvent('keydown', function(event){
+    if(event.key == "right"){
+        move_char();
+    }
+});
+
+function move_char(){
+    var req = new Request({
+        url: 'test.txt',
+        onSuccess: function(res){
+            var char_left = parseInt($('character').getStyle("left"))
+            $('character').setStyle('left', char_left + 1 + "px")
+        }
+    }).send();
+}
+
+function create_char(){
+    //$('map_container')
+
+}
